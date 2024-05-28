2c251fa05192efb944a99a0c2c11e33656c84c5f
Prabhas Kurapati <prabhask@berkeley.edu>
2024-05-28 04:25:15 -0400

    [BF-6] fixed navbar navigation styling

--- src/app/(app)/_components/Navbar.tsx
+++ src/app/(app)/_components/Navbar.tsx
@@ -13,16 +13,10 @@ import {
   IconButton,
   useDisclosure,
   Drawer,
-  DrawerBody,
   DrawerContent,
-  DrawerHeader,
   DrawerOverlay,
-  DrawerCloseButton,
-  Button,
-  DrawerFooter,
-  Stack,
 } from "@chakra-ui/react";
-import { HamburgerIcon } from "@chakra-ui/icons";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 import Image from "next/image";
 import Link from "next/link";
 import placeholder from "@/media/avatar_placeholder.svg";
@@ -86,12 +80,129 @@ export default function NavBar({ profilePic, email, name }: NavBarProps) {
     </div>
   );
 
-  const dropdownBody = () => {
-    const menuOptionsBaseStyle = "font-[510]";
-    const menuOptionsCurrentPageStyleAddOn = " text-[#A73CFC]";
-    const menuOptionsSelectStyleAddOn =
-      " cursor-pointer hover:bg-[#F2F2F2] active:bg-[#E6E6E6]";
+  const menuOptionsBaseStyle =
+    "font-[510] flex items-center p-2 group cursor-pointer ";
+  const menuOptionsCurrentPageStyleAddOn = " text-[#A73CFC]";
+  const svgBaseStyle = "flex-shrink-0 w-5 h-5 ";
+  const svgSelectStyleAddOn = " text-gray-500";
+  const menuOptionsSelectStyleAddOn = " hover:bg-[#F2F2F2] active:bg-[#E6E6E6]";
 
+  const exploreOption = (rounded: boolean) => (
+    <a
+      onClick={() => handleRedirect("/explore")}
+      className={
+        menuOptionsBaseStyle +
+        (pathname === "/explore"
+          ? menuOptionsCurrentPageStyleAddOn
+          : menuOptionsSelectStyleAddOn) +
+        (rounded ? " rounded-lg" : "")
+      }
+    >
+      <svg
+        className={
+          svgBaseStyle +
+          (pathname == "/explore"
+            ? menuOptionsCurrentPageStyleAddOn
+            : svgSelectStyleAddOn)
+        }
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 20 18"
+      >
+        <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
+      </svg>
+      <span className="flex-1 ms-3 whitespace-nowrap">Explore</span>
+    </a>
+  );
+
+  const profileOption = (rounded: boolean) => (
+    <a
+      onClick={() => handleRedirect("/profile")}
+      className={
+        menuOptionsBaseStyle +
+        (pathname === "/profile"
+          ? menuOptionsCurrentPageStyleAddOn
+          : menuOptionsSelectStyleAddOn) +
+        (rounded ? " rounded-lg" : "")
+      }
+    >
+      <svg
+        className={
+          svgBaseStyle +
+          (pathname == "/profile"
+            ? menuOptionsCurrentPageStyleAddOn
+            : svgSelectStyleAddOn)
+        }
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 18 20"
+      >
+        <path d="M16 0H4a2 2 0 0 0-2 2v1H1a1 1 0 0 0 0 2h1v2H1a1 1 0 0 0 0 2h1v2H1a1 1 0 0 0 0 2h1v2H1a1 1 0 0 0 0 2h1v1a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4.5a3 3 0 1 1 0 6 3 3 0 0 1 0-6ZM13.929 17H7.071a.5.5 0 0 1-.5-.5 3.935 3.935 0 1 1 7.858 0 .5.5 0 0 1-.5.5Z" />
+      </svg>
+      <span className="flex-1 ms-3 whitespace-nowrap">My Profile</span>
+    </a>
+  );
+
+  const requestsOption = (rounded: boolean) => (
+    <a
+      onClick={() => handleRedirect("/requests")}
+      className={
+        menuOptionsBaseStyle +
+        (pathname === "/requests"
+          ? menuOptionsCurrentPageStyleAddOn
+          : menuOptionsSelectStyleAddOn) +
+        (rounded ? " rounded-lg" : "")
+      }
+    >
+      <svg
+        className={
+          svgBaseStyle +
+          (pathname == "/requests"
+            ? menuOptionsCurrentPageStyleAddOn
+            : svgSelectStyleAddOn)
+        }
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+      >
+        <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z" />
+      </svg>
+      <span className="flex-1 ms-3 whitespace-nowrap">My Requests</span>
+    </a>
+  );
+
+  const signOutOption = (rounded: boolean) => (
+    <a
+      onClick={() => signOut({ callbackUrl: "/login" })}
+      className={
+        menuOptionsBaseStyle +
+        menuOptionsSelectStyleAddOn +
+        (rounded ? " rounded-lg" : "")
+      }
+    >
+      <svg
+        className="flex-shrink-0 w-5 h-5 text-gray-500"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 16 16"
+      >
+        <path
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M4 8h11m0 0-4-4m4 4-4 4m-5 3H3a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h3"
+        />
+      </svg>
+      <span className="flex-1 ms-3 whitespace-nowrap">Sign out</span>
+    </a>
+  );
+
+  const dropdownBody = () => {
     return (
       <PopoverContent className="w-48">
         <PopoverArrow />
@@ -100,46 +211,11 @@ export default function NavBar({ profilePic, email, name }: NavBarProps) {
           <div className="font-bold truncate">{email}</div>
         </PopoverBody>
         <Divider />
-        <PopoverBody
-          onClick={() => handleRedirect("/explore")}
-          className={
-            menuOptionsBaseStyle +
-            (pathname === "/explore"
-              ? menuOptionsCurrentPageStyleAddOn
-              : menuOptionsSelectStyleAddOn)
-          }
-        >
-          Explore
-        </PopoverBody>
-        <PopoverBody
-          onClick={() => handleRedirect("/profile")}
-          className={
-            menuOptionsBaseStyle +
-            (pathname === "/profile"
-              ? menuOptionsCurrentPageStyleAddOn
-              : menuOptionsSelectStyleAddOn)
-          }
-        >
-          My Profile
-        </PopoverBody>
-        <PopoverBody
-          onClick={() => handleRedirect("/requests")}
-          className={
-            menuOptionsBaseStyle +
-            (pathname === "/requests"
-              ? menuOptionsCurrentPageStyleAddOn
-              : menuOptionsSelectStyleAddOn)
-          }
-        >
-          My Requests
-        </PopoverBody>
+        {exploreOption(false)}
+        {profileOption(false)}
+        {requestsOption(false)}
         <Divider />
-        <PopoverBody
-          onClick={() => signOut({ callbackUrl: "/login" })}
-          className={menuOptionsBaseStyle + menuOptionsSelectStyleAddOn}
-        >
-          Sign out
-        </PopoverBody>
+        {signOutOption(false)}
       </PopoverContent>
     );
   };
@@ -165,77 +241,40 @@ export default function NavBar({ profilePic, email, name }: NavBarProps) {
     />
   );
 
-  const drawer = () => {
-    const menuOptionsBaseStyle = "font-[510] pl-6";
-    const menuOptionsCurrentPageStyleAddOn = " text-[#A73CFC]";
-    const menuOptionsSelectStyleAddOn =
-      " cursor-pointer hover:bg-[#F2F2F2] active:bg-[#E6E6E6]";
+  const closeButton = (
+    <IconButton
+      className="rounded-full"
+      variant="ghost"
+      onClick={onClose}
+      icon={<CloseIcon className="w-3 h-3" />}
+      aria-label="Close drawer button"
+    />
+  );
 
-    return (
-      <Drawer onClose={onClose} isOpen={isOpen} size="xs">
-        <DrawerOverlay />
-        <DrawerContent>
-          <DrawerCloseButton />
-          <DrawerHeader className="mt-5">
-            <div className="flex flex-row">
-              <div className="my-auto">{logo}</div>
-              <div className="ml-auto my-auto">{avatar}</div>
-            </div>
-          </DrawerHeader>
-          <DrawerBody className="p-0 mt-8">
-            <div className="pl-6">
-              <div className="font-bold">{name}</div>
-              <div className="font-bold truncate">{email}</div>
-            </div>
-            <Stack className="mt-10" spacing={[2, 2, 2, 2, 2, 2]}>
-              <div
-                onClick={() => handleRedirect("/explore")}
-                className={
-                  menuOptionsBaseStyle +
-                  (pathname === "/explore"
-                    ? menuOptionsCurrentPageStyleAddOn
-                    : menuOptionsSelectStyleAddOn)
-                }
-              >
-                Explore
-              </div>
-              <div
-                onClick={() => handleRedirect("/profile")}
-                className={
-                  menuOptionsBaseStyle +
-                  (pathname === "/profile"
-                    ? menuOptionsCurrentPageStyleAddOn
-                    : menuOptionsSelectStyleAddOn)
-                }
-              >
-                My Profile
-              </div>
-              <div
-                onClick={() => handleRedirect("/requests")}
-                className={
-                  menuOptionsBaseStyle +
-                  (pathname === "/requests"
-                    ? menuOptionsCurrentPageStyleAddOn
-                    : menuOptionsSelectStyleAddOn)
-                }
-              >
-                My Requests
-              </div>
-            </Stack>
-            <DrawerFooter>
-              <Button
-                className="mr-auto mt-10"
-                variant="websiteTheme"
-                onClick={() => signOut({ callbackUrl: "/login" })}
-              >
-                Sign Out
-              </Button>
-            </DrawerFooter>
-          </DrawerBody>
-        </DrawerContent>
-      </Drawer>
-    );
-  };
+  const drawer = () => (
+    <Drawer onClose={onClose} isOpen={isOpen} size="xs">
+      <DrawerOverlay />
+      <DrawerContent>
+        <div className="flex flex-row my-6 px-6">
+          <div className="my-auto">{logo}</div>
+          <div className="ml-auto my-auto">{closeButton}</div>
+        </div>
+        <div className="px-6 flex flex-row my-6">
+          <div className="my-auto">
+            <div className="font-bold">{name}</div>
+            <div className="font-bold truncate">{email}</div>
+          </div>
+          <div className="my-auto ml-auto">{avatar}</div>
+        </div>
+        <div className="px-4 my-3">
+          {exploreOption(true)}
+          {profileOption(true)}
+          {requestsOption(true)}
+          {signOutOption(true)}
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
 
   return isTabletOrMobile ? (
     <>
