7b9d424ca2a765e5a8f7550a19433f4f1a078dbf
Prabhas Kurapati <prabhask@berkeley.edu>
2024-06-02 04:18:42 -0700

    [BF-6] rm console logs

--- src/lib/test.config.ts
+++ src/lib/test.config.ts
@@ -4,9 +4,7 @@ import testData from "@/data/testData";
 export const InitiateManyTestData = async () => {
   try {
     await User.insertMany(testData);
-    console.log("inserted many user test data!");
   } catch (e) {
-    console.log(e);
     throw e;
   }
 };
@@ -14,9 +12,7 @@ export const InitiateManyTestData = async () => {
 export const InitiateSingleTestData = async () => {
   try {
     await User.create(testData);
-    console.log("inserted single user test data!");
   } catch (e) {
-    console.log(e);
     throw e;
   }
 };
