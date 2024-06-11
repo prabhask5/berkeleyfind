3f96a4068721797cdaf7bc8f55e0cea1d6df6c99
Prabhas Kurapati <prabhask@berkeley.edu>
2024-06-10 22:34:51 -0700

    [BF-7] configure prune unused dependency workflow

---
+++ src/lib/removeUnusedDependencies.mjs
@@ -0,0 +1,39 @@
+/* eslint-disable no-console */
+import exec from "child_process";
+import depcheck from "depcheck";
+
+const options = {
+  ignoreDirs: [
+    // list of directories to ignore
+    "node_modules",
+    "dist",
+    "build",
+  ],
+  ignoreMatches: [
+    // list of modules to ignore
+  ],
+};
+
+depcheck(process.cwd(), options, (unused) => {
+  const unusedDependencies = unused.dependencies;
+  const unusedDevDependencies = unused.devDependencies;
+
+  if (unusedDependencies.length === 0 && unusedDevDependencies.length === 0) {
+    console.log("No unused dependencies found.");
+    return;
+  }
+
+  const allUnused = [...unusedDependencies, ...unusedDevDependencies];
+  console.log(`Unused dependencies: ${allUnused.join(", ")}`);
+
+  const uninstallCmd = `npm uninstall ${allUnused.join(" ")}`;
+  console.log(`Running: ${uninstallCmd}`);
+
+  exec(uninstallCmd, (err, stdout, stderr) => {
+    if (err) {
+      console.error(`Error uninstalling packages: ${stderr}`);
+    } else {
+      console.log(`Successfully uninstalled unused packages:\n${stdout}`);
+    }
+  });
+});
